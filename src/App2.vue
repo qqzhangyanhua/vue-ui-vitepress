0af85d4b79c8e77d9e54f75574b85b3eb47c4de7
张艳华 <939593210@qq.com>
2024-01-27 16:28:00 +0800

    fff

---
+++ src/App2.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue';
+import Test from './Test';
+</script>
+
+<template>
+	<div>
+		<Test />
+	</div>
+
+	<HelloWorld msg="Vite + Vue" />
+</template>
+
+<style scoped></style>
